cb14a47e464605ef630680761332b04b3c988652
undg <und3rdg@gmail.com>
2024-11-07 22:52:36 +0000

    separate controller

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LoadingOrError from './components/loading-or-error'
 import About from './pages/about'
 import { Config } from './pages/config'
-import { ControllerInput, ControllerOutput } from './pages/controller'
+import { ControllerOutput } from './pages/controller-output'
+import { ControllerInput } from './pages/controller-input'
 
 export default function App(): ReactElement {
 	return (
