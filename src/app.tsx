8496ef67f592a590c7ddcb995ac914ff64e34380
undg <und3rdg@gmail.com>
2024-11-10 15:12:52 +0000

    extract router

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,14 @@
 import type { ReactElement } from 'react'
 import { Suspense } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import LoadingOrError from './components/loading-or-error'
-import About from './pages/about'
-import { Config } from './pages/config'
-import { ControllerOutput } from './pages/controller-output'
-import { ControllerInput } from './pages/controller-input'
+import { Router } from './router'
 
 export default function App(): ReactElement {
 	return (
 		<BrowserRouter>
 			<Suspense fallback={<LoadingOrError />}>
-				<Routes>
-					<Route path='/' element={<ControllerOutput />} />
-					<Route path='/input' element={<ControllerInput />} />
-					<Route path='/about' element={<About />} />
-					<Route path='/config' element={<Config />} />
-				</Routes>
+				<Router />
 			</Suspense>
 		</BrowserRouter>
 	)
